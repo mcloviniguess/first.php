fa718f53c59ca37f37abfcb7a0d95db370d7eb2a
mcloviniguess <keyogvosn@gmail.com>
2025-07-11 08:36:21 +0800

    Add files via upload

---
+++ first.php
@@ -0,0 +1,8 @@
+<?php
+ echo "My name is: Kellychen Aniate<br>"; 
+ echo "I am 18 years old<br>";
+ echo "My favorite subject is Computer Programming<br>";
+ echo "My dream job is Software Engineer<br>";
+ echo "My motto in life is 'LET GO AND LET GOD'<br> ";
+ echo "~(^._.)"
+?> 
